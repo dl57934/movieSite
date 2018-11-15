0b56bf5ecc1d9752c86a6da9de28702c584e4936
dl57934 <dl57934@pukyong.ac.kr>
2018-11-16 07:56:05 +0900

    setting React Router

--- moviefront/src/javascript/App.js
+++ moviefront/src/javascript/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import client from './apolloClient';
 import { ApolloProvider } from 'react-apollo';
+import {HashRouter, Route} from 'react-router-dom';
+import Home from './Home';
+import Detail from './detail';
 
 class App extends Component {
     render() {
         return (
         <ApolloProvider client={client}>
-            <div className="App"/>
+            <HashRouter>
+                <Fragment>
+                    <Route exact={true} path={'/'} component={Home}/>
+                    <Route path={'/details/:movieId'} component={Detail}/>
+                </Fragment>
+            </HashRouter>
         </ApolloProvider>
         )
     }
---
+++ moviefront/src/javascript/detail.js
@@ -0,0 +1,5 @@
+import react from 'react';
+
+const Detail = ()=> "detail";
+
+export default Detail

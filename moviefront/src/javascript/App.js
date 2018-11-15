3dfebf1271709b0aabb23c9104c95c1a97a28bc0
dl57934 <dl57934@pukyong.ac.kr>
2018-11-16 07:36:19 +0900

    making client

---
+++ moviefront/src/javascript/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import client from './apolloClient';
+import { ApolloProvider } from 'react-apollo';
+
+class App extends Component {
+    render() {
+        return (
+        <ApolloProvider client={client}>
+            <div className="App"/>
+        </ApolloProvider>
+        )
+    }
+}
+
+export default App;
